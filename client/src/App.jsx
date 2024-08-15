f1cb3a2114cea24a579a1579ee49be431fa4db29
Cole Maring <65455664+colemaring@users.noreply.github.com>
2024-08-15 17:36:41 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import ConnectedClients from "./components/ConnectedClients";
 
 function App() {
   // Establish WebSocket connection
-  const wsUrl = "ws://localhost:8080";
+  const wsUrl = "ws://143.244.157.174:8080";
   const ws = new WebSocket(wsUrl);
 
   return (
