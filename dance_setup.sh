2f955f96281e51c16555dc55f8a5c87dbcbff8be
Chinmay Nehate <42030910+chinmaynehate@users.noreply.github.com>
2024-08-22 11:27:58 -0400

    Update dance_setup.sh

--- dance_setup.sh
+++ dance_setup.sh
@@ -2,7 +2,7 @@ git clone https://github.com/unitreerobotics/unitree_legged_sdk.git
 cd unitree_legged_sdk
 mkdir build
 cd build
-pip3 install catkin_pkg
+pip3 install catkin_pkg websocket-client
 sudo apt install libmsgpack*
 cmake -DPYTHON_BUILD=TRUE ..
 make
